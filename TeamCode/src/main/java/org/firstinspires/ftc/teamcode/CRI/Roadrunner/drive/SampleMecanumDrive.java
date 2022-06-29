46f60ffb77ff7cf526080cf315a5ddb84a0eba33
plexcodes <ignotusplex@gmail.com>
2022-06-29 16:54:40 +0300

    Drive constants ready.
    Former-commit-id: 523957d327ad317d7c6bfcdccfa60c29fe7bc9a2
    Former-commit-id: d4d9ad7e4e4a90493081d523bef74030dc00c136
    Former-commit-id: f97dbac9ba1af84ba09a3685f9ce962af683d465
    Former-commit-id: 0513f455af539e5cf742c8e9c813578cefdb0c06

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRI/Roadrunner/drive/SampleMecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CRI/Roadrunner/drive/SampleMecanumDrive.java
@@ -56,8 +56,8 @@ import static org.firstinspires.ftc.teamcode.CRI.Roadrunner.drive.DriveConstants
  */
 @Config
 public class SampleMecanumDrive extends MecanumDrive {
-    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(0, 0, 0);
-    public static PIDCoefficients HEADING_PID = new PIDCoefficients(0, 0, 0);
+    public static PIDCoefficients TRANSLATIONAL_PID = new PIDCoefficients(6, 0, 0);
+    public static PIDCoefficients HEADING_PID = new PIDCoefficients(4, 0, 0);
 
     public static double LATERAL_MULTIPLIER = 1.215;
 
